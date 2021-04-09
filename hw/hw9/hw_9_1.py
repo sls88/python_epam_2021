69e606e4dc8f1cfc7ec5268d596a7a6f86c053e2
Dmitry Z <sls8@ya.ru>
2021-04-09 20:00:32 +0300

    Homework 9.1. Test corrections

--- hw/hw9/hw_9_1.py
+++ hw/hw9/hw_9_1.py
@@ -6,21 +6,6 @@ from typing import Iterator, List, Union
 
 
 def merge_sorted_files(lst: List[Union[Path, str]]) -> Iterator:
-    """Merge integer from 2 sorted files and return an iterator.
-
-    Args:
-        lst: paths
-
-    Returns:
-        The return value. Iterator object
-    """
-    path1, path2 = Path(lst[0]), Path(lst[1])
-    with open(path1) as f1, open(path2) as f2:
-        a = [[int(i), int(j)] for i, j in zip(f1, f2)]
-    return iter(sum(a, []))
-
-
-def merge_sorted_inf_seq(lst: List[Union[Path, str]]) -> Iterator:
     """Merge integer from infinite number of sorted sequences in files and return an iterator.
 
     Args:
@@ -32,4 +17,4 @@ def merge_sorted_inf_seq(lst: List[Union[Path, str]]) -> Iterator:
     with ExitStack() as stack:
         files = [stack.enter_context(open(fname)) for fname in lst]
         fin = list(zip(*map(lambda x: map(int, list(x)), files)))
-        return iter([j for i in fin for j in i])
+        return iter(sorted(j for i in fin for j in i))
--- test/hw9/test_hw9_1.py
+++ test/hw9/test_hw9_1.py
@@ -4,57 +4,33 @@ from typing import List, Union
 
 import pytest
 
-from hw.hw9.hw_9_1 import merge_sorted_files, merge_sorted_inf_seq
+from hw.hw9.hw_9_1 import merge_sorted_files
 
 
-@pytest.fixture()
-def double_path() -> List[Union[Path, str]]:
-    path1 = Path("test/hw9/test_task1/double1.txt")
-    path2 = Path("test/hw9/test_task1/double2.txt")
-    return [path1, path2]
+def p() -> str:
+    return "test/hw9/test_task1/"
 
 
-@pytest.fixture()
-def different_lengths() -> List[Union[Path, str]]:
-    path1 = Path("test/hw9/test_task1/double1.txt")
-    path2 = Path("test/hw9/test_task1/broken.txt")
-    return [path1, path2]
+@pytest.mark.parametrize(
+    ("paths", "expected_result"),
+    [
+        ([Path(p() + i) for i in ("1_2_3.txt", "4_5_6.txt")], [1, 2, 3, 4, 5, 6]),
+        ([Path(p() + i) for i in ("1_3_5.txt", "2_4_6.txt")], [1, 2, 3, 4, 5, 6]),
+        (
+            [Path(p() + i) for i in ("1_4_7.txt", "2_5_8.txt", "3_6_9.txt")],
+            [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        ),
+        ([Path(p() + i) for i in ("4_5_6.txt", "1_1_1_1.txt")], [1, 1, 1, 4, 5, 6]),
+    ],
+)
+def test_backspace_recursion_variant(
+    paths: List[Union[Path, str]], expected_result: List
+):
+    actual_result = list(merge_sorted_files(paths))
 
-
-@pytest.fixture()
-def triple_path() -> List[Union[Path, str]]:
-    path1 = Path("test/hw9/test_task1/file1.txt")
-    path2 = Path("test/hw9/test_task1/file2.txt")
-    path3 = Path("test/hw9/test_task1/file3.txt")
-    return [path1, path2, path3]
-
-
-def test_merge_sorted_files(double_path):
-    actual_result = list(merge_sorted_files(double_path))
-
-    assert actual_result == [1, 2, 3, 4, 5, 6]
-
-
-def test_different_lengths(different_lengths):
-    actual_result = list(merge_sorted_files(different_lengths))
-
-    assert actual_result == [1, 1, 3, 1, 5, 1]
+    assert actual_result == expected_result
 
 
 def test_file_not_found():
     with pytest.raises(FileNotFoundError):
         list(merge_sorted_files(["ggg.ty", "kkk.ru"]))
-
-
-def test_merge_sorted_inf_seq(triple_path, double_path):
-    actual_result = list(merge_sorted_inf_seq(triple_path))
-    actual_result2 = list(merge_sorted_inf_seq(double_path))
-
-    assert actual_result == [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    assert actual_result2 == [1, 2, 3, 4, 5, 6]
-
-
-def test_diff_lengths_sorted_inf_seq_func(different_lengths):
-    actual_result = list(merge_sorted_inf_seq(different_lengths))
-
-    assert actual_result == [1, 1, 3, 1, 5, 1]
